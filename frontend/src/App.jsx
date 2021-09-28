11a8a269d4f4c1b48da969bced481b07dbb2370e
Shubham401 <shubham26.bdps@gmail.com>
2021-09-28 21:08:16 +0530

    Frontend-Backend Communication

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,39 @@
 import React from 'react';
 import { StreamChat } from 'stream-chat';
 import { Chat } from 'stream-chat-react';
-//import Cookies from 'universal-cookie';
+import Cookies from 'universal-cookie';
 
 import { ChannelListContainer, ChannelContainer, Auth } from './components';
 
 import './App.css';
 
+const cookies = new Cookies();
+const authToken = cookies.get("token");
+
 const apiKey = 'jnhrsgfa94vc';
 const client = StreamChat.getInstance(apiKey);
-const authToken = false;
+
+if (authToken) {
+    client.connectUser({
+        id: cookies.get('userId'),
+        name: cookies.get('username'),
+        fullName: cookies.get('fullName'),
+        image: cookies.get('avatarURL'),
+        hashedPassword: cookies.get('hashedPassword'),
+        phoneNumber: cookies.get('phoneNumber'),
+    }, authToken)
+}
 
 const App = () => {
-    if(!authToken) return <Auth />
+    if (!authToken) return <Auth />
     return (
         <div className="app__wrapper">
             <Chat client={client} theme="team light">
                 <ChannelListContainer
-                
+
                 />
-                <ChannelContainer 
-                
+                <ChannelContainer
+
                 />
             </Chat>
         </div>
